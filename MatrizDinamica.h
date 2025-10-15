8122f216ff86114e7e09a76676fc900ad063f5d2
Diego Zapata <2430053@upv.edu.mx>
2025-10-14 18:02:56 -0600

    Clase matriz flexible implementada.

--- MatrizDinamica.h
+++ MatrizDinamica.h
@@ -1,10 +1,214 @@
+#include <stdexcept>
+
 template <typename T>
-class MatrizDinamica {
+class MatrizDinamica
+{
 private:
     T **datos;
     int filas;
     int columnas;
 
 public:
-    // Constructor, Destructor, Métodos de gestión y operaciones
+    // Getters
+    int getFilas() const { return filas; }
+    int getColumnas() const { return columnas; }
+    T **getDatos() const { return datos; }
+
+    // Constructor; Es llamado para realizar una instancia. (Constructor)
+    MatrizDinamica(int f, int c)
+        : datos(nullptr), filas(f), columnas(c)
+    {
+        // Crear matriz al instanciar clase
+        this->datos = createMatiz(this->filas, this->columnas);
+    }
+    // Destructor; Es llamado al destruirse el objeto (Destructor)
+    ~MatrizDinamica()
+    {
+        this->deleteMatriz(this->datos, this->filas, this->columnas);
+    };
+    // Copiar mediante constructor (Copy Constructor)
+
+    MatrizDinamica(const MatrizDinamica<T> &other)
+        : datos(nullptr), filas(0), columnas(0)
+    {
+        // 1. Crear copia de la matriz que contiene el objeto source
+        this->datos = this->copyMatriz(other.getDatos(), other.getFilas(), other.getColumnas());
+
+        // 2. Actualizar filas y columnas
+        this->filas = other.getFilas();
+        this->columnas = other.getColumnas();
+    }
+
+    // Copia los valores de un objeto al objeto. (Copy Assigment Operator)
+    MatrizDinamica &operator=(const MatrizDinamica<T> &other)
+    {
+        if (this != &other)
+        {
+            // 1. Eliminar matriz actual
+            this->deleteMatriz(this->datos, this->filas, this->columnas);
+            this->datos = nullptr;
+
+            // 2. Crear copia de la matriz que contiene el objeto source
+            this->datos = this->copyMatriz(other.getDatos(), other.getFilas(), other.getColumnas());
+
+            // 3. Actualizar filas y columnas
+            this->filas = other.getFilas();
+            this->columnas = other.getColumnas();
+        }
+        return *this;
+    }
+    // Mover los datos de un objeto a un nuevo objeto (Move Constructor)
+    MatrizDinamica(MatrizDinamica<T> &&other) noexcept
+        // 1. Asignar los datos del otro objeot al objeto nuevo.
+        : datos(other.datos), filas(other.filas), columnas(other.columnas)
+    {
+        // 2. Desenlazar/eliminar los datos del otro objeto.
+        other.datos = nullptr;
+        other.filas = 0;
+        other.columnas = 0;
+    }
+
+    // Mover los datos de otro objeto a al objeto. (Move Assigment Operator)
+    MatrizDinamica &operator=(MatrizDinamica<T> &&other) noexcept
+    {
+        if (this != &other)
+        {
+            // 1. Eliminar datos actuales
+            deleteMatriz(this->datos, this->filas, this->columnas);
+
+            // 2. Mover los datos del otro objeto a este objeto.
+            this->datos = other.datos;
+            this->filas = other.filas;
+            this->columnas = other.columnas;
+
+            // 3. Desenlazar/eliminar los datos del otro objeto.
+            other.datos = nullptr;
+            other.filas = 0;
+            other.columnas = 0;
+        }
+        return *this;
+    }
+
+    void redimensionar(int new_filas, int new_columnas)
+    {
+        // validar tamaño positivo
+        if (new_filas < 0 || new_columnas < 0)
+            throw std::invalid_argument("No es posible una medida negativo");
+
+        // 1. Crear nueva matriz e inicializar con valores 0
+        T **new_datos = createMatiz(new_filas, new_columnas);
+
+        // 2. Copiar valores originales a la nueva matriz
+
+        // 2.1 Definir cual es el limite a copiar
+        int max_cpy_filas = (new_filas > this->filas) ? this->filas : new_filas;
+        int max_cpy_columnas = (new_columnas > this->columnas) ? this->columnas : new_columnas;
+
+        // 2.2 Copiar valores
+        for (int i = 0; i < max_cpy_filas; i++)
+        {
+            for (int j = 0; j < max_cpy_columnas; j++)
+                new_datos[i][j] = this->datos[i][j];
+        }
+
+        // 4. Eliminar anterior matriz
+        this->deleteMatriz(this->datos, this->filas, this->columnas);
+        this->datos = nullptr;
+
+        // 5. Remplazamos el puntero
+        this->datos = new_datos;
+
+        // 6. Despunteamos el auxiliar
+        new_datos = nullptr;
+
+        // 7. actualizamos medidas
+        this->filas = new_filas;
+        this->columnas = new_columnas;
+    }
+
+    template <typename U, typename P>
+    static MatrizDinamica<T> *multiplicar(const MatrizDinamica<U> &A, const MatrizDinamica<P> &B)
+    {
+        // Validar si los tipos de dato se permiten operaciones.
+
+        // 1. Validar si se pueden multiplicar
+        if (A.getColumnas() != B.getFilas())
+        {
+            throw std::invalid_argument("Las matrices no se pueden multiplicar");
+        }
+
+        // Formula  A(m x n) * B(n x p) = C(m x p)
+        int m = A.getFilas();
+        int n = A.getColumnas();
+        int p = B.getColumnas();
+
+        // 2. Crear matriz auxiliar con las nuevas medidas
+        MatrizDinamica<T> *C = new MatrizDinamica<T>(m, p);
+
+        // 3. Intentar Realizar multiplicacion
+        try
+        {
+
+            for (int i = 0; i < C->getFilas(); i++)
+            {
+                for (int j = 0; j < C->getColumnas(); j++)
+                {
+                    T result = 0;
+                    for (int k = 0; k < n; k++)
+                    {
+                        result += A.getDatos()[i][k] * B.getDatos()[k][j];
+                    }
+                    C->getDatos()[i][j] = result;
+                }
+            }
+        }
+        catch (const std::exception &e)
+        {
+            // Ante algun fallo Informar del error.
+            throw std::invalid_argument("Lost tipos de datos son imposibles de multiplicar");
+        }
+        return C;
+    }
+
+private:
+    void deleteMatriz(T **matriz, int filas, int columnas = 0)
+    {
+        for (int i = 0; i < filas; i++)
+            delete[] matriz[i];
+        delete[] matriz;
+    }
+
+    T **createMatiz(int filas, int columnas)
+    {
+        // intentar crear una matriz
+        try
+        {
+            // 1. Crear nueva matriz e inicializar con valores 0
+            T **matriz = new T *[filas];
+            for (int i = 0; i < filas; i++)
+            {
+                matriz[i] = new T[columnas];
+                for (int j = 0; j < columnas; j++)
+                    matriz[i][j] = 0;
+            };
+            return matriz;
+        }
+        catch (std::bad_alloc)
+        {
+            // logger->log("Not enough memory to create the video layer.");
+            throw;
+        }
+    };
+    T **copyMatriz(T **matriz, int filas, int columnas)
+    {
+        T **matriz_cpy = createMatiz(filas, columnas);
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                matriz_cpy[i][j] = matriz[i][j];
+            }
+        }
+        return matriz_cpy;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,126 @@
 #include <iostream>
+#include <iomanip>
+#include <limits>
 #include "MatrizDinamica.h"
 
-int main() {
+// Imprimir matriz
+template <typename T>
+void print_matriz(MatrizDinamica<T> matriz);
+
+// Imprimir mensaje
+void print_message(std::string msg);
+
+// Obtener Inputs de un tipo de dato flexible:
+template <typename T>
+T getInput(const std::string prompt);
+
+template <>
+std::string getInput<std::string>(const std::string prompt);
+
+int main()
+{
+    print_message("--- Sistema de Análisis de Transformaciones Lineales ---");
+    print_message(">> Prueba de Matriz ENTERA (Matriz A) <<");
+    print_message("Creando Matriz A (Tipo INT) de 2x3...");
+    print_message("Ingresar valores para A (2x3):");
+    MatrizDinamica<int> *matrizA = new MatrizDinamica<int>(2, 3);
+    for (int i = 0; i < matrizA->getFilas(); i++)
+    {
+        for (int j = 0; j < matrizA->getColumnas(); j++)
+        {
+            std::string prompt = "A";
+            prompt.append("[" + std::to_string(i) + "]"); // filas
+            prompt.append("[" + std::to_string(j) + "]"); // columnas
+            prompt.append(":");
+            int value = getInput<int>(prompt);
+            matrizA->getDatos()[i][j] = value;
+        }
+    }
+    print_message(">> Redimensionando Matriz A <<");
+    matrizA->redimensionar(3, 3);
+    print_message("Redimensionando A a 3x3. Datos conservados:");
+    print_matriz(*matrizA);
+
+    print_message(">> Prueba de Multiplicación (Tipo FLOAT) <<");
+    print_message("Ingresar valores para B (Tipo FLOAT) de 3x2...");
+    MatrizDinamica<float> *matrizB = new MatrizDinamica<float>(3, 2);
+    for (int i = 0; i < matrizB->getFilas(); i++)
+    {
+        for (int j = 0; j < matrizB->getColumnas(); j++)
+        {
+            std::string prompt = "B";
+            prompt.append("[" + std::to_string(i) + "]"); // filas
+            prompt.append("[" + std::to_string(j) + "]"); // columnas
+            prompt.append(":");
+            float value = getInput<float>(prompt);
+            matrizB->getDatos()[i][j] = value;
+        }
+    }
+    print_matriz(*matrizB);
+    MatrizDinamica<float> *matrizC = MatrizDinamica<float>::multiplicar(*matrizA, *matrizB);
+
+    print_message("Matriz C = A(3x3) x B(3x2) ...");
+    print_message("Matriz C (Resultado 3x2, Tipo FLOAT):");
+    print_matriz(*matrizC);
+
+    print_message("Liberando memoria de todas las matrices...");
+
+    delete matrizA;
+    delete matrizB;
+    delete matrizC;
+    print_message("Memoria liberada exitosamente!, Bye!");
     return 0;
+}
+
+template <typename T>
+void print_matriz(MatrizDinamica<T> matriz)
+{
+    for (int i = 0; i < matriz.getFilas(); i++)
+    {
+        for (int j = 0; j < matriz.getColumnas(); j++)
+        {
+            std::string celda = (j != matriz.getColumnas() - 1) ? "|" : "";
+            celda.append(std::to_string(matriz.getDatos()[i][j]));
+            celda.append("|");
+            std::cout << celda;
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <typename T>
+T getInput(const std::string prompt)
+{
+    T input;
+    std::cout << prompt;
+    while (!(std::cin >> input))
+    {
+        std::cout << "Ingrese un input valido!" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << prompt;
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return input;
+}
+
+template <>
+std::string getInput(const std::string prompt)
+{
+    std::cout << prompt;
+    std::string temp;
+    std::getline(std::cin, temp);
+    return temp;
+}
+
+void print_message(std::string msg)
+{
+    const std::string MENU_TOP_ROUND = "╭─────────────────────────────────────────────────────╮";
+    const std::string MENU_BORDER = "│";
+    const std::string MENU_BOT_ROUND = "╰─────────────────────────────────────────────────────╯";
+    const int content_width = 50;
+    std::cout << MENU_TOP_ROUND << std::endl;
+    std::string formatted_msg = " " + msg + " ";
+    std::cout << MENU_BORDER << std::left << std::setw(content_width) << formatted_msg << std::endl;
+    std::cout << MENU_BOT_ROUND << std::endl;
 }
